9fc63a574ec917455ee5a064f30912543dc6094a
neintendo <kambashaalfredo@gmail.com>
2025-09-03 12:00:48 +0200

    task 3.1

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Author(models.Model):
 
@@ -24,3 +25,14 @@ class Librarian(models.Model):
 
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+class UserProfile(models.Model):
+
+    ROLES = [
+        ("admin", "Admin"),
+        ("librarian", "Librarian"),
+        ("member", "Member")
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(choices=ROLES)
