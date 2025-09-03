657c6c7c250cbf2e9c74f7c870a95de8cc016a2c
neintendo <kambashaalfredo@gmail.com>
2025-09-03 11:27:35 +0200

    templates fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
-from .views import list_books
 from .views import LibraryDetailView
-from django.contrib.auth.views import LoginView
-from django.urls import path
-from django.contrib.auth.views import LogoutView
+from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
 from relationship_app import views
 
 urlpatterns = [
+    path('books/', list_books, name = 'list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('register/', views.register.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -27,4 +27,4 @@ class LibraryDetailView(DetailView):
 class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/registration.html'
+    template_name = 'relationship_app/register.html'
